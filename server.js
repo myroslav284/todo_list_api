e6fb3714372a6edb5a9019c49ea68b32a94ae16f
myroslav284 <miroslav2267@gmail.com>
2023-04-22 21:17:43 +0300

    connect to db

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require("express");
+const connectDB = require("./db/connect");
+require("dotenv").config();
 const app = express();
-
-const PORT = 3000;
+app.use(express.json());
+const PORT = process.env.PORT || 3001;
 
 app.get('/api', (req, res)=>{
     res.send("Successful")
 })
 
-app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+const start = async ()=>{
+    try {
+        await connectDB('mongodb+srv://admin:admin@cluster0.imbp9t9.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+            app.listen(PORT, console.log(`Server is running on port ${PORT}`));
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
